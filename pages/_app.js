4d3b83be97e0b31ef3a3d9734a7ed45250f944cf
Testando0 <joh769637@gmail.com>
2026-03-10 14:17:39 -0300

    Register service worker in App component
    Added service worker registration in useEffect.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }) {
+  // Register service worker
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
+      <div className="nav-spacer" />
       <Component {...pageProps} />
     </>
   );
