35dc8d2a5bc67f4574e8cdbaf8fb502b51775fb4
Testando0 <joh769637@gmail.com>
2026-03-10 13:39:27 -0300

    Update meta tags and favicon in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,12 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <meta name="description" content="freeTube - Assista YouTube sem anúncios" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' rx='18' fill='%23ff3d3d'/><polygon points='38,28 78,50 38,72' fill='white'/></svg>" />
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#080608" />
+        <meta name="description" content="RubiTube — Assista YouTube sem anúncios, sem rastreamento." />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><rect width='100' height='100' rx='20' fill='%23c0152a'/><polygon points='36,25 76,50 36,75' fill='white'/></svg>" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
