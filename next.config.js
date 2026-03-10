6f9a95774104e1b26b20bfe0e3489d0d79a55b36
Testando0 <joh769637@gmail.com>
2026-03-10 13:43:24 -0300

    Update image remote patterns and configuration
    Removed a remote pattern for images and added a new one. Also disabled image optimization and removed custom headers configuration.

--- next.config.js
+++ next.config.js
@@ -4,19 +4,10 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**.ytimg.com' },
       { protocol: 'https', hostname: '**.ggpht.com' },
-      { protocol: 'https', hostname: 'invidious.kavin.rocks' },
       { protocol: 'https', hostname: 'i.ytimg.com' },
+      { protocol: 'https', hostname: '**.googleusercontent.com' },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-        ],
-      },
-    ];
+    unoptimized: true,
   },
 };
 
